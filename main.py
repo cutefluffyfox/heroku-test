ab172df01d72f82e61a2961ad7bd358e0adba377
cutefluffyfox <mishkazel1@gmail.com>
2020-03-17 18:03:35 +0300

    /hello page added

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def index():
     return "Привет от приложения Flask"
 
 
+@app.route("/hello")
+def hello():
+    return "Лол ок"
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
